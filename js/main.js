acf7aaa3cf86a2a8ae76202b612fae7fe94b324f
vahidghaiem <vahidghaiem297@gmail.com>
2023-01-16 03:10:49 +0330

    init

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  $('.responsive1').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    rtl:true,
+    arrows:false,
+    autoplay:true,
+    pauseOnHover:true,
+    infinite:true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots:false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  
+                });
+            
+              
